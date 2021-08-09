38be92f8b188601f67393d0d4adc729ec765d66d
s-dow <40843634+s-dow@users.noreply.github.com>
2021-08-09 19:10:14 -0400

    added db connection

--- models/db.js
+++ models/db.js
@@ -1,8 +1,34 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(`postgres://hackupstate@localhost:5432/events`, {
-  logging: false,
-});
+//
+let db;
+
+let dbURL = process.env.DATABASE_URL;
+if (!dbURL) {
+  db = new Sequelize("postgres://hackupstate@localhost:5432/events", {
+    logging: false,
+    dialect: "postgres",
+    protocol: "postgres",
+  });
+} else {
+  db = new Sequelize(dbURL, {
+    logging: false,
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+}
+
+//
+
+// const db = new Sequelize(`postgres://hackupstate@localhost:5432/events`, {
+//   logging: false,
+// });
 
 const Event = require("./Event")(db);
 const User = require("./User")(db);
