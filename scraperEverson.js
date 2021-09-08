ad12e9c20c60a8a7eb1f06f6fb46e1c35651c355
s-dow <40843634+s-dow@users.noreply.github.com>
2021-09-07 20:36:26 -0400

    check

--- scraperEverson.js
+++ scraperEverson.js
@@ -8,7 +8,7 @@ const { db } = require("./models/db");
 const allEventsUrl = "https://everson.org/events-list/events-category-events/";
 const oneEventUrl = "https://everson.org/connect/";
 
-cron.schedule("* * */1 * *", async () => {
+cron.schedule("*/1 * * * *", async () => {
   const response = await fetch(`${allEventsUrl}`);
   const body = await response.text();
   const $ = cheerio.load(body);
